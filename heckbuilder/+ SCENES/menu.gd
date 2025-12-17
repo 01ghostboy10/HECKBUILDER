82d4a65c96627816d94c938de553349d55afd0ec
01ghostboy10 <levana.rai.simcik@mensa.cz>
2025-12-17 22:11:36 +0100

    animations

---
+++ heckbuilder/+ SCENES/menu.gd
@@ -0,0 +1,27 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+#-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.#
+
+
+
+
+func _on_settings_mouse_entered() -> void:
+	$settings/AnimationPlayer.play("move")
+func _on_settings_mouse_exited() -> void:
+	$settings/AnimationPlayer.play("return")
+
+#-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.#
+
+func _on_start_mouse_entered() -> void:
+	$start/AnimationPlayer.play("move")
+func _on_start_mouse_exited() -> void:
+	$start/AnimationPlayer.play("return")
